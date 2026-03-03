7d8ce4c77a5008a94654cefd7bcf344d4d8a58a2
Variant <praisekaye13@gmail.com>
2026-03-03 23:50:17 +0100

    Added smooth scroll in JS

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
